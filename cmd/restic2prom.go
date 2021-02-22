0022f886c329b28ddc38052030aa57642ff5b43a
Juergen Hoetzel <juergen@hoetzel.info>
2021-02-22 20:05:53 +0100

    (Mis)use cobra to get RESTIC_REPOSITORY from cmd line flags or env

--- cmd/restic2prom.go
+++ cmd/restic2prom.go
@@ -1,30 +1,55 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
-	"os/exec"
+	"github.com/juergenhoetzel/restic2prom/metrics"
+	"github.com/spf13/cobra"
 	"os"
-	"github.com/juergenhoetzel/restic2prom/internal/util"
+	"os/exec"
 	"strings"
 )
 
 func main() {
 	args := make([]string, len(os.Args))
-	copy(args, os.Args[2:])
-	if ! util.Contains(args, "--json") {
-		args = append([]string{"--json"}, args...)
+	var prom *metrics.Prom
+	// we leverage cobra to get the repository from restic flags or environment variables
+	rootCmd := &cobra.Command{
+		Use: "restic",
+		Run: func(cmd *cobra.Command, args []string) {
+			repo, _ := cmd.Flags().GetString("repo")
+			// FIXME
+			prom = metrics.New(repo, "/tmp/out.prom")
+		},
+		FParseErrWhitelist: cobra.FParseErrWhitelist{
+			UnknownFlags: true,
+		},
 	}
+	rootCmd.PersistentFlags().StringP("repo", "r", os.Getenv("RESTIC_REPOSITORY"), "_")
+	rootCmd.Execute()
+	copy(args, os.Args[2:])
 	cmd := exec.Command(os.Args[1], args...)
 	cmd.Stdin = os.Stdin
-	cmd.Stdout = os.Stdout
-	cmd.Stderr = os.Stderr
+	stdoutPipe, _ := cmd.StdoutPipe()
+	stdoutReader := bufio.NewReader(stdoutPipe)
+	stderrPipe, _ := cmd.StderrPipe()
+	stderrReader := bufio.NewReader(stderrPipe)
+
 	cmd.Start()
+
+	go prom.CollectStdout(stdoutReader)
+	go prom.CollectStderr(stderrReader)
+
 	err := cmd.Wait()
+	if !prom.WriteToTextFile() {
+		fmt.Fprintf(os.Stderr, "Did not receive JSON metrics. Missed to add '--json' flag?\n")
+	}
+	// FIXME: Write metrics in error case?
 	if err != nil {
 		if _, ok := err.(*exec.ExitError); ok {
 			os.Exit(cmd.ProcessState.ExitCode())
 		} else {
-			fmt.Fprintf(os.Stderr, "exec '%s' failed\n", strings.Join(os.Args[1:], " ") )
+			fmt.Fprintf(os.Stderr, "exec '%s' failed\n", strings.Join(os.Args[1:], " "))
 			os.Exit(1)
 		}
 	}
--- go.mod
+++ go.mod
@@ -2,4 +2,8 @@ module github.com/juergenhoetzel/restic2prom
 
 go 1.15
 
-require github.com/prometheus/client_golang v1.9.0
+require (
+	github.com/prometheus/client_golang v1.9.0
+	github.com/spf13/cobra v0.0.3
+	github.com/spf13/pflag v1.0.1
+)
